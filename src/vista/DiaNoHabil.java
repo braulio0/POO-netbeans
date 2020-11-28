00c4b45bf7e4d111aa360a8c99ad9f3329b5e9d6
emmasvl <56358382+emmasvl@users.noreply.github.com>
2020-11-28 13:46:33 -0600

    Add JFrame DiaNoHabil

--- src/vista/DiaNoHabil.java
+++ src/vista/DiaNoHabil.java
@@ -37,11 +37,8 @@ public class DiaNoHabil extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jTextField2 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -83,24 +80,21 @@ public class DiaNoHabil extends javax.swing.JFrame {
         getContentPane().add(jLabel4);
         jLabel4.setBounds(90, 210, 170, 14);
 
-        jLabel5.setText("Fecha ");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(80, 240, 32, 14);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Fecha ", "Motivo ", "Status"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable1);
 
-        jLabel6.setText("Motivo ");
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(190, 240, 40, 14);
-
-        jLabel7.setText("Status");
-        getContentPane().add(jLabel7);
-        jLabel7.setBounds(300, 240, 31, 20);
-
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
-
-        getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(60, 230, 340, 180);
+        getContentPane().add(jScrollPane3);
+        jScrollPane3.setBounds(40, 230, 452, 90);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -148,13 +142,10 @@ public class DiaNoHabil extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
